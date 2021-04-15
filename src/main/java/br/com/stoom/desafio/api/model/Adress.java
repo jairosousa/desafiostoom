ba1b0cd21be1e80630df5e3d9d8203181c5dd4f2
jairosousa <jaironsousa@gmail.com>
2021-04-15 18:39:22 -0300

    :construction: criação model Adress

---
+++ src/main/java/br/com/stoom/desafio/api/model/Adress.java
@@ -0,0 +1,57 @@
+package br.com.stoom.desafio.api.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Adress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "streetName is mandatory")
+    @Column
+    private String streetName;
+
+    @NotBlank(message = "number is mandatory")
+    @Column
+    private String number;
+
+    @Column
+    private String complement;
+
+    @NotBlank(message = "neighbourhood is mandatory")
+    @Column
+    private String neighbourhood;
+
+    @NotBlank(message = "city is mandatory")
+    @Column
+    private String city;
+
+    @NotBlank(message = "state is mandatory")
+    @Column
+    private String state;
+
+    @NotBlank(message = "country is mandatory")
+    @Column
+    private String country;
+
+    @NotBlank(message = "zipcode is mandatory")
+    @Column
+    private String zipcode;
+
+    @Column
+    private String latitude;
+
+    @Column
+    private String longitude;
+
+}
